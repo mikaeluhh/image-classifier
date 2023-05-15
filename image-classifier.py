823efff2e2df7a4c3a2a498f05579ae19da0b5b2
mikaeluhh <133606251+mikaeluhh@users.noreply.github.com>
2023-05-15 17:30:14 +0800

    Add files via upload

---
+++ image-classifier.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import tensorflow as tf
+import tensorflow_hub as hub
+
+# Function to load the pre-trained model
+@st.cache_resource
+def load_model():
+    model = tf.keras.models.load_model('weights-improvement-10-0.91.hdf5', custom_objects={'KerasLayer': hub.KerasLayer})
+    return model
+
+# Set the page title and description
+st.set_page_config(page_title="Time of the Day Classification", page_icon="🌞")
+st.title("Time of the Day Classification")
+st.markdown("---")
+st.write("This website classifies the time of day based on your uploaded image.")
+
+# File uploader
+file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+# Function to preprocess and predict the image
+def import_and_predict(image_data, model):
+    size = (224, 224)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    image = np.asarray(image)
+    image = image / 255.0
+    img_reshape = np.reshape(image, (1, 224, 224, 3))
+    prediction = model.predict(img_reshape)
+    return prediction
+
+# Load the pre-trained model
+model = load_model()
+class_names = ['Day time', 'Night time', 'Sunrise']
+
+# Display prediction results
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    class_index = np.argmax(prediction)
+    class_label = class_names[class_index]
+    string = "Prediction: " + class_label
+    st.success(string)
